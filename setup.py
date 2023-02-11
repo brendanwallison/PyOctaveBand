db1f364553b07962e6b1be400d5e6cd3ef27ba1d
brendanwallison <57687858+brendanwallison@users.noreply.github.com>
2023-02-10 20:33:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='PyOctaveBand (Pip Installable Fork)',
    version='1.0',
    description='Module for calculating octave bands of audio files',
-   author='https://github.com/jmrplens'
+   author='https://github.com/jmrplens',
    packages=['PyOctaveBand'],  
-   install_requires=['numpy', 'matplotlib', 'scipy'],
+   install_requires=['numpy', 'matplotlib', 'scipy']
 )
